5166bedd0dc90e015f486436866b1d3de981679b
墨染_nlx <78503566+morannlx@users.noreply.github.com>
2026-01-11 20:37:35 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,9 +98,9 @@ TW_INCLUDE_ZSTD               := true
 
 # TWRP display
 TW_BRIGHTNESS_PATH      := /sys/class/backlight/panel0-backlight/brightness
-TW_DEFAULT_BRIGHTNESS   := 500
+TW_DEFAULT_BRIGHTNESS   := 1300
 TW_FRAMERATE            := 60
-#TW_MAX_BRIGHTNESS       := 4095
+TW_MAX_BRIGHTNESS       := 4095
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_THEME := portrait_hdpi
